a885b5271f1fba9c9d60d3b04667df98a2038b36
Braden Everson <bradeneverson@gmail.com>
2024-04-19 09:32:05 -0500

    Many models

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,38 @@
 use std::{error::Error, path::Path};
 
 use eden_ai::data::plantstate::PlantState;
-use unda::core::{data::input::Input, network::Network, layer::{layers::{InputTypes, LayerTypes}, methods::activations::Activations}};
+use unda::core::{data::input::Input, network::Network, layer::{layers::{InputTypes, LayerTypes}, methods::{activations::Activations, errors::ErrorTypes}}};
 
 fn main() -> Result<(), Box<dyn Error>>{
-    let plants = get_data("filled_plants.json")?;
-    let mut inputs: Vec<&dyn Input> = vec![];
-    let mut outputs = vec![];
+    loop {
+        let plants = get_data("plants.json")?;
+        let mut inputs: Vec<&dyn Input> = vec![];
+        let mut outputs = vec![];
 
-    let data_tuple: Vec<(Vec<f32>, Vec<f32>)> = 
-        plants.iter().map(|plant| plant.data_tuple()).collect();
+        let data_tuple: Vec<(Vec<f32>, Vec<f32>)> = 
+            plants.iter().map(|plant| plant.data_tuple()).collect();
 
-    data_tuple.iter().for_each(|(inp, out)| {
-        inputs.push(inp);
-        outputs.push(out);
-    });
+        data_tuple.iter().for_each(|(inp, out)| {
+            inputs.push(inp);
+            outputs.push(out.clone());
+        });
 
-    let mut plant_model = Network::new(128);
-    
-    plant_model.set_input(InputTypes::DENSE(data_tuple[0].0.len()));
+        let mut plant_model = Network::new(128);
 
-    plant_model.add_layer(LayerTypes::DENSE(16, Activations::RELU, 0.001));
-    plant_model.add_layer(LayerTypes::DENSE(32, Activations::RELU, 0.001));
+        plant_model.set_input(InputTypes::DENSE(data_tuple[0].0.len()));
 
-    plant_model.add_layer(LayerTypes::DENSE(1, Activations::SIGMOID, 0.001));
+        plant_model.add_layer(LayerTypes::DENSE(8, Activations::RELU, 0.001));
+        plant_model.add_layer(LayerTypes::DENSE(32, Activations::RELU, 0.001));
+        plant_model.add_layer(LayerTypes::DENSE(16, Activations::RELU, 0.001));
 
-    plant_model.compile();
+        plant_model.add_layer(LayerTypes::DENSE(1, Activations::SIGMOID, 0.001));
 
-    plant_model.serialize_unda_fmt("plant_model_3_16_32_1.unda");
+        plant_model.compile();
 
-    Ok(())
+        plant_model.fit(&inputs, &outputs, 5, ErrorTypes::CategoricalCrossEntropy);
+
+        plant_model.serialize_unda_fmt(&format!("plant_data_{:.2}.unda", plant_model.loss * 100f32));
+    }
 }
 
 fn create_data() -> Result<(), Box<dyn Error>>{
