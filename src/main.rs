d2173a7efd60f91404aeae70c5f96e48a4e335b8
Braden Everson <bradeneverson@gmail.com>
2024-04-19 06:19:23 -0500

    Create model for watering plants

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,34 @@
 use std::{error::Error, path::Path};
 
 use eden_ai::data::plantstate::PlantState;
+use unda::core::{data::input::Input, network::Network, layer::{layers::{InputTypes, LayerTypes}, methods::activations::Activations}};
 
 fn main() -> Result<(), Box<dyn Error>>{
-    create_data()?;
+    let plants = get_data("filled_plants.json")?;
+    let mut inputs: Vec<&dyn Input> = vec![];
+    let mut outputs = vec![];
+
+    let data_tuple: Vec<(Vec<f32>, Vec<f32>)> = 
+        plants.iter().map(|plant| plant.data_tuple()).collect();
+
+    data_tuple.iter().for_each(|(inp, out)| {
+        inputs.push(inp);
+        outputs.push(out);
+    });
+
+    let mut plant_model = Network::new(128);
+    
+    plant_model.set_input(InputTypes::DENSE(data_tuple[0].0.len()));
+
+    plant_model.add_layer(LayerTypes::DENSE(16, Activations::RELU, 0.001));
+    plant_model.add_layer(LayerTypes::DENSE(32, Activations::RELU, 0.001));
+
+    plant_model.add_layer(LayerTypes::DENSE(1, Activations::SIGMOID, 0.001));
+
+    plant_model.compile();
+
+    plant_model.serialize_unda_fmt("plant_model_3_16_32_1.unda");
+
     Ok(())
 }
 
