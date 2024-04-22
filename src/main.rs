aaf8dc52ee0f949d30c02e38d0e1d7f3f20583cc
Braden Everson <bradeneverson@gmail.com>
2024-04-22 07:52:02 -0500

    Model training loop for sun

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,36 @@ use unda::core::{data::input::Input, network::Network, layer::{layers::{InputTyp
 fn main() -> Result<(), Box<dyn Error>>{
 
     loop {
+        let sun = get_sun("sun.json")?; 
+        let mut inputs: Vec<&dyn Input> = vec![];
+        let mut outputs = vec![];
+
+        let data_tuple: Vec<(Vec<f32>, Vec<f32>)> = sun.iter().map(|sun| sun.data_tuple()).collect();
+
+        data_tuple.iter().for_each(|(inp, out)| {
+            inputs.push(inp);
+            outputs.push(out.clone());
+        });
+
+        let mut sun_model = Network::new(128);
+
+        sun_model.set_input(InputTypes::DENSE(4));
+
+        sun_model.add_layer(LayerTypes::DENSE(64, Activations::SIGMOID, 0.001));
+        sun_model.add_layer(LayerTypes::DENSE(32, Activations::SIGMOID, 0.001));
+        sun_model.add_layer(LayerTypes::DENSE(8, Activations::SIGMOID, 0.001));
+
+        sun_model.add_layer(LayerTypes::DENSE(1, Activations::SIGMOID, 0.001));
+
+        sun_model.compile();
+
+        sun_model.fit(&inputs, &outputs, 5, ErrorTypes::CategoricalCrossEntropy);
+
+        sun_model.serialize_unda_fmt(&format!("sun_data_{:.2}_sigmoid.unda", sun_model.loss * 100f32));
+        
+        /*
         let plants = get_data("plants.json")?;
+
         let mut inputs: Vec<&dyn Input> = vec![];
         let mut outputs = vec![];
 
@@ -33,6 +62,7 @@ fn main() -> Result<(), Box<dyn Error>>{
         plant_model.fit(&inputs, &outputs, 5, ErrorTypes::CategoricalCrossEntropy);
 
         plant_model.serialize_unda_fmt(&format!("plant_data_{:.2}_sigmoid.unda", plant_model.loss * 100f32));
+        */
     }
 }
 
@@ -58,4 +88,9 @@ fn create_data() -> Result<(), Box<dyn Error>>{
 
 fn get_data<P: AsRef<Path>>(path: P) -> Result<Vec<PlantState>, Box<dyn Error>> {
     PlantState::get_plants(path)
+}
+
+
+fn get_sun<P: AsRef<Path>>(path: P) -> Result<Vec<SunState>, Box<dyn Error>> {
+    SunState::get(path)
 }
