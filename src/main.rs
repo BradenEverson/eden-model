cbc4acc00731f56a238dd97186dcfc461150a01b
Braden Everson <bradeneverson@gmail.com>
2024-04-22 05:50:50 -0500

    Sun data to train

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::{error::Error, path::Path};
 
-use eden_ai::data::plantstate::PlantState;
+use eden_ai::data::plantstate::{PlantState, SunState};
 use unda::core::{data::input::Input, network::Network, layer::{layers::{InputTypes, LayerTypes}, methods::{activations::Activations, errors::ErrorTypes}}};
 
 fn main() -> Result<(), Box<dyn Error>>{
+
+    create_data_sun()
+    /*
     loop {
         let plants = get_data("plants.json")?;
         let mut inputs: Vec<&dyn Input> = vec![];
@@ -32,7 +35,17 @@ fn main() -> Result<(), Box<dyn Error>>{
         plant_model.fit(&inputs, &outputs, 5, ErrorTypes::CategoricalCrossEntropy);
 
         plant_model.serialize_unda_fmt(&format!("plant_data_{:.2}_sigmoid.unda", plant_model.loss * 100f32));
+    }*/
+}
+
+fn create_data_sun() -> Result<(), Box<dyn Error>>{
+    let mut plants: Vec<SunState> = vec![];
+    for i in 0..200 {
+        plants.push(SunState::new(i));
     }
+    SunState::save(&plants, "sundata.json")?;
+    println!("Done");
+    Ok(())
 }
 
 fn create_data() -> Result<(), Box<dyn Error>>{
